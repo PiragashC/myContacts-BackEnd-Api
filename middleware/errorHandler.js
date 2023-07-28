c9f3b174e4e3123a0c8e9e1046b745854f51f565
Piragash <pragashconstantine13@gmail.com>
2023-07-28 10:31:19 +0530

    fourth commit

---
+++ middleware/errorHandler.js
@@ -0,0 +1,36 @@
+const { constants} = require("../constants");
+const errorHandler = (err, req, res, next) => {
+    const statusCode = res.statusCode ? res.statusCode : 500;
+    switch (statusCode) {
+        case constants.VALIDATION_ERROR:
+            res.json({titile : "Validation Failed", 
+            message : err.message, 
+            stackTrace: err.stack});
+            break;
+        case constants.NOT_FOUND:
+            res.json({titile : "Not Found", 
+            message : err.message, 
+            stackTrace: err.stack});
+            break;
+        case constants.UNAUTHORIZED:
+            res.json({titile : "Unauthorized", 
+            message : err.message, 
+            stackTrace: err.stack});
+            break;
+        case constants.FORBIDDEN:
+            res.json({titile : "Forbidden", 
+            message : err.message, 
+            stackTrace: err.stack});
+            break;
+        case constants.SERVER_ERROR:
+            res.json({titile : "Server Error", 
+            message : err.message, 
+            stackTrace: err.stack});
+            break;
+        default:
+            console.log("No  Error, All good!");
+            break;
+    }    
+};
+
+module.exports = errorHandler;
